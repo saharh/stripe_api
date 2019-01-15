bde9b4a3a88cd4612ec0ce94b129c0dd3f3c90b4
Sahar Haddad <saharh09@gmail.com>
2019-01-16 01:36:01 +0200

    card utils

--- lib/card_utils.dart
+++ lib/card_utils.dart
@@ -14,148 +14,155 @@ const int MAX_LENGTH_COMMON = 19;
 // because Diners Club has one more space, but one less digit.
 const int MAX_LENGTH_AMEX_DINERS = 17;
 
-/**
- * Checks the input string to see whether or not it is a valid card number, possibly
- * with groupings separated by spaces or hyphens.
- *
- * @param cardNumber a String that may or may not represent a valid card number
- * @return {@code true} if and only if the input value is a valid card number
- */
-bool isValidCardNumber(String cardNumber) {
-  String normalizedNumber = removeSpacesAndHyphens(cardNumber);
-  return isValidLuhnNumber(normalizedNumber) && isValidCardLength(normalizedNumber);
-}
-
-bool isValidExpMonth(int expMonth) {
-  return expMonth != null && expMonth >= 1 && expMonth <= 12;
-}
-
-bool isValidExpYear(int expYear, {DateTime now}) {
-  now = now ?? DateTime.now();
-  return expYear != null && !ModelUtils.hasYearPassed(expYear, now);
-}
-
-bool isValidExpiryDate(int expYear, int expMonth, {DateTime now}) {
-  now = now ?? DateTime.now();
-  if (!isValidExpMonth(expMonth)) {
-    return false;
-  }
-  if (!isValidExpYear(expYear)) {
-    return false;
-  }
-  return !ModelUtils.hasMonthPassed(expYear, expMonth, now);
-}
-
-bool isValidCVC(String cvc, String brand) {
-  if (isBlank(cvc)) {
-    return false;
-  }
-  String cvcValue = cvc.trim();
-  String updatedType = brand;
-  bool validLength = (updatedType == null && cvcValue.length >= 3 && cvcValue.length <= 4) ||
-      (StripeCard.AMERICAN_EXPRESS == updatedType && cvcValue.length == 4) ||
-      cvcValue.length == 3;
-
-  return ModelUtils.isWholePositiveNumber(cvcValue) && validLength;
-}
-
-/**
- * Checks the input string to see whether or not it is a valid Luhn number.
- *
- * @param cardNumber a String that may or may not represent a valid Luhn number
- * @return {@code true} if and only if the input value is a valid Luhn number
- */
-bool isValidLuhnNumber(String cardNumber) {
-  if (cardNumber == null) {
-    return false;
+class CardUtils {
+  /**
+   * Checks the input string to see whether or not it is a valid card number, possibly
+   * with groupings separated by spaces or hyphens.
+   *
+   * @param cardNumber a String that may or may not represent a valid card number
+   * @return {@code true} if and only if the input value is a valid card number
+   */
+  bool isValidCardNumber(String cardNumber) {
+    String normalizedNumber = removeSpacesAndHyphens(cardNumber);
+    return isValidLuhnNumber(normalizedNumber) && isValidCardLength(normalizedNumber);
   }
 
-  bool isOdd = true;
-  int sum = 0;
+  bool isValidExpMonth(int expMonth) {
+    return expMonth != null && expMonth >= 1 && expMonth <= 12;
+  }
 
-  for (int index = cardNumber.length - 1; index >= 0; index--) {
-    var c = cardNumber[index];
-    if (!isDigit(c)) {
+  bool isValidExpYear(int expYear, {DateTime now}) {
+    now = now ?? DateTime.now();
+    return expYear != null && !ModelUtils.hasYearPassed(expYear, now);
+  }
+
+  bool isValidExpiryDate(int expYear, int expMonth, {DateTime now}) {
+    now = now ?? DateTime.now();
+    if (!isValidExpMonth(expMonth)) {
+      return false;
+    }
+    if (!isValidExpYear(expYear)) {
+      return false;
+    }
+    return !ModelUtils.hasMonthPassed(expYear, expMonth, now);
+  }
+
+  bool isValidCVC(String cvc, String brand) {
+    if (isBlank(cvc)) {
+      return false;
+    }
+    String cvcValue = cvc.trim();
+    String updatedType = brand;
+    bool validLength = (updatedType == null && cvcValue.length >= 3 && cvcValue.length <= 4) ||
+        (StripeCard.AMERICAN_EXPRESS == updatedType && cvcValue.length == 4) ||
+        cvcValue.length == 3;
+
+    return ModelUtils.isWholePositiveNumber(cvcValue) && validLength;
+  }
+
+  List<int> getExpiryDate(String value) {
+    var split = value.split(new RegExp(r'(\/)'));
+    return [int.parse(split[0]), int.parse(split[1])];
+  }
+
+  /**
+   * Checks the input string to see whether or not it is a valid Luhn number.
+   *
+   * @param cardNumber a String that may or may not represent a valid Luhn number
+   * @return {@code true} if and only if the input value is a valid Luhn number
+   */
+  bool isValidLuhnNumber(String cardNumber) {
+    if (cardNumber == null) {
       return false;
     }
 
-    int digitInteger = getNumericValue(c);
-    isOdd = !isOdd;
+    bool isOdd = true;
+    int sum = 0;
 
-    if (isOdd) {
-      digitInteger *= 2;
+    for (int index = cardNumber.length - 1; index >= 0; index--) {
+      var c = cardNumber[index];
+      if (!isDigit(c)) {
+        return false;
+      }
+
+      int digitInteger = getNumericValue(c);
+      isOdd = !isOdd;
+
+      if (isOdd) {
+        digitInteger *= 2;
+      }
+
+      if (digitInteger > 9) {
+        digitInteger -= 9;
+      }
+
+      sum += digitInteger;
     }
 
-    if (digitInteger > 9) {
-      digitInteger -= 9;
+    return sum % 10 == 0;
+  }
+
+  /**
+   * Checks to see whether the input number is of the correct length, given the assumed brand of
+   * the card. This function does not perform a Luhn check.
+   *
+   * @param cardNumber the card number with no spaces or dashes
+   * @param cardBrand a {@link CardBrand} used to get the correct size
+   * @return {@code true} if the card number is the correct length for the assumed brand
+   */
+  bool isValidCardLength(String cardNumber, {String cardBrand}) {
+    if (cardBrand == null) {
+      cardBrand = getPossibleCardType(cardNumber, shouldNormalize: false);
+    }
+    if (cardNumber == null || StripeCard.UNKNOWN == cardBrand) {
+      return false;
     }
 
-    sum += digitInteger;
+    int length = cardNumber.length;
+    switch (cardBrand) {
+      case StripeCard.AMERICAN_EXPRESS:
+        return length == LENGTH_AMERICAN_EXPRESS;
+      case StripeCard.DINERS_CLUB:
+        return length == LENGTH_DINERS_CLUB;
+      default:
+        return length == LENGTH_COMMON_CARD;
+    }
   }
 
-  return sum % 10 == 0;
-}
+  String getPossibleCardType(String cardNumber, {bool shouldNormalize = true}) {
+    if (isBlank(cardNumber)) {
+      return StripeCard.UNKNOWN;
+    }
 
-/**
- * Checks to see whether the input number is of the correct length, given the assumed brand of
- * the card. This function does not perform a Luhn check.
- *
- * @param cardNumber the card number with no spaces or dashes
- * @param cardBrand a {@link CardBrand} used to get the correct size
- * @return {@code true} if the card number is the correct length for the assumed brand
- */
-bool isValidCardLength(String cardNumber, {String cardBrand}) {
-  if (cardBrand == null) {
-    cardBrand = getPossibleCardType(cardNumber, shouldNormalize: false);
-  }
-  if (cardNumber == null || StripeCard.UNKNOWN == cardBrand) {
-    return false;
-  }
+    String spacelessCardNumber = cardNumber;
+    if (shouldNormalize) {
+      spacelessCardNumber = removeSpacesAndHyphens(cardNumber);
+    }
 
-  int length = cardNumber.length;
-  switch (cardBrand) {
-    case StripeCard.AMERICAN_EXPRESS:
-      return length == LENGTH_AMERICAN_EXPRESS;
-    case StripeCard.DINERS_CLUB:
-      return length == LENGTH_DINERS_CLUB;
-    default:
-      return length == LENGTH_COMMON_CARD;
-  }
-}
-
-String getPossibleCardType(String cardNumber, {bool shouldNormalize = true}) {
-  if (isBlank(cardNumber)) {
-    return StripeCard.UNKNOWN;
+    if (hasAnyPrefix(spacelessCardNumber, StripeCard.PREFIXES_AMERICAN_EXPRESS)) {
+      return StripeCard.AMERICAN_EXPRESS;
+    } else if (hasAnyPrefix(spacelessCardNumber, StripeCard.PREFIXES_DISCOVER)) {
+      return StripeCard.DISCOVER;
+    } else if (hasAnyPrefix(spacelessCardNumber, StripeCard.PREFIXES_JCB)) {
+      return StripeCard.JCB;
+    } else if (hasAnyPrefix(spacelessCardNumber, StripeCard.PREFIXES_DINERS_CLUB)) {
+      return StripeCard.DINERS_CLUB;
+    } else if (hasAnyPrefix(spacelessCardNumber, StripeCard.PREFIXES_VISA)) {
+      return StripeCard.VISA;
+    } else if (hasAnyPrefix(spacelessCardNumber, StripeCard.PREFIXES_MASTERCARD)) {
+      return StripeCard.MASTERCARD;
+    } else if (hasAnyPrefix(spacelessCardNumber, StripeCard.PREFIXES_UNIONPAY)) {
+      return StripeCard.UNIONPAY;
+    } else {
+      return StripeCard.UNKNOWN;
+    }
   }
 
-  String spacelessCardNumber = cardNumber;
-  if (shouldNormalize) {
-    spacelessCardNumber = removeSpacesAndHyphens(cardNumber);
-  }
-
-  if (hasAnyPrefix(spacelessCardNumber, StripeCard.PREFIXES_AMERICAN_EXPRESS)) {
-    return StripeCard.AMERICAN_EXPRESS;
-  } else if (hasAnyPrefix(spacelessCardNumber, StripeCard.PREFIXES_DISCOVER)) {
-    return StripeCard.DISCOVER;
-  } else if (hasAnyPrefix(spacelessCardNumber, StripeCard.PREFIXES_JCB)) {
-    return StripeCard.JCB;
-  } else if (hasAnyPrefix(spacelessCardNumber, StripeCard.PREFIXES_DINERS_CLUB)) {
-    return StripeCard.DINERS_CLUB;
-  } else if (hasAnyPrefix(spacelessCardNumber, StripeCard.PREFIXES_VISA)) {
-    return StripeCard.VISA;
-  } else if (hasAnyPrefix(spacelessCardNumber, StripeCard.PREFIXES_MASTERCARD)) {
-    return StripeCard.MASTERCARD;
-  } else if (hasAnyPrefix(spacelessCardNumber, StripeCard.PREFIXES_UNIONPAY)) {
-    return StripeCard.UNIONPAY;
-  } else {
-    return StripeCard.UNKNOWN;
-  }
-}
-
-int getLengthForBrand(String cardBrand) {
-  if (StripeCard.AMERICAN_EXPRESS == cardBrand || StripeCard.DINERS_CLUB == cardBrand) {
-    return MAX_LENGTH_AMEX_DINERS;
-  } else {
-    return MAX_LENGTH_COMMON;
+  int getLengthForBrand(String cardBrand) {
+    if (StripeCard.AMERICAN_EXPRESS == cardBrand || StripeCard.DINERS_CLUB == cardBrand) {
+      return MAX_LENGTH_AMEX_DINERS;
+    } else {
+      return MAX_LENGTH_COMMON;
+    }
   }
 }
