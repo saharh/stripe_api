d64b4a67f4f8920e0dc970df3de34bacb1a9cde2
Sahar Haddad <saharh09@gmail.com>
2019-02-10 20:59:33 +0200

    ios

--- ios/Classes/SwiftStripeApiPlugin.swift
+++ ios/Classes/SwiftStripeApiPlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import Stripe
 
 public class SwiftStripeApiPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -9,6 +10,42 @@ public class SwiftStripeApiPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    if call.method == "init" {
+        STPPaymentConfiguration.shared().publishableKey = call.arguments as! String
+//        STPPaymentConfiguration.shared().appleMerchantIdentifier = "your apple merchant identifier"
+        result(nil)
+    } else if call.method == "createSource" {
+        let args = call.arguments as! [String:Any]
+        let cardParams = STPCardParams.init()
+        cardParams.number = args["number"] as? String;
+        cardParams.expMonth = args["exp_month"] as? UInt ?? 0
+        cardParams.expYear = args["exp_year"] as? UInt ?? 0
+        cardParams.cvc = args["cvc"] as? String
+        cardParams.name = args["name"] as? String
+        let address = STPAddress.init()
+        address.line1 = args["address_line1"] as? String
+        address.line2 = args["address_line2"] as? String
+        address.city = args["address_city"] as? String
+        address.state = args["address_state"] as? String
+        address.postalCode = args["address_zip"] as? String
+        address.country = args["address_country"] as? String
+        cardParams.address = address
+        let sourceParams = STPSourceParams.cardParams(withCard:cardParams)
+        let sourceCallback = { (source:STPSource?, error: Error?) -> Void in
+            if (error != nil) {
+                result(FlutterError(code: error?.localizedDescription ?? "", message: nil, details: nil))
+            }
+            result(self.sourceToDict(source: source!))
+        }
+        STPAPIClient.shared().createSource(with: sourceParams, completion: sourceCallback)
+
+    } else {
+        result(FlutterMethodNotImplemented)
+    }
   }
+    
+    func sourceToDict(source: STPSource) -> [AnyHashable:Any?] {
+        let ret = source.allResponseFields
+        return ret
+    }
 }
