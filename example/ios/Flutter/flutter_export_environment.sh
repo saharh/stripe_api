1bf22c58485177cf961cdd8fd0a57643b538a7b1
Sahar Haddad <saharh09@gmail.com>
2019-09-23 14:50:12 +0300

    updated libs

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\Develop\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Develop\wabi\mobile\flutter_plugins\stripe_api\example"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\Develop\flutter\bin\cache\artifacts\engine\ios"
